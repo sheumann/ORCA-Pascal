2ae5e7f5ad52bf5a36a3b65bd45cf590e1a457cf
Stephen Heumann <stephenheumann@gmail.com>
2025-03-12 21:30:56 -0500

    Fix bug where comparing 32-bit values in global arrays or structs against 0 may give wrong results with large memory model.
    The issue was that 16-bit absolute addressing (in the data bank) was being used to access the data to compare, but with the large memory model the global arrays or structs are not necessarily in the same bank, so absolute long addressing should be used.
    
    This corresponds to byteworksinc/ORCA-C@709f9b3f25c7beb6ded5fbaca560c0fa0a49f468.
    
    The following program often demonstrates the problem (depending on memory layout and contents):
    
    {$memorymodel 1}
    {$optimize 1}
    program largemmbug(output);
    
    var
            ch1: packed array[1..32000] of char;
            L1: array[0..1] of longint;
    
    begin
    L1[0] := 0;
    if L1[0] <> 0 then
            writeln('bug detected'); {depends on memory layout and contents}
    end.

--- native.pas
+++ native.pas
@@ -601,8 +601,8 @@ case p_opcode of
    m_adc_abs,m_adc_dir,m_adc_imm,m_adc_s,m_and_abs,m_and_dir,m_and_imm,
    m_and_s,m_asl_a,m_dea,m_eor_abs,m_eor_dir,m_eor_imm,m_eor_s,m_lda_absx,
    m_lda_dirx,m_lda_indl,m_lda_indly,m_lda_longx,m_lda_s,m_lsr_a,m_ora_abs,
-   m_ora_dir,m_ora_dirX,m_ora_imm,m_ora_longX,m_ora_s,m_pla,m_sbc_abs,
-   m_sbc_dir,m_sbc_imm,m_sbc_s,m_tdc,m_tsc,m_tsb_dir,m_tsb_abs:
+   m_ora_dir,m_ora_dirX,m_ora_imm,m_ora_long,m_ora_longX,m_ora_s,m_pla,
+   m_sbc_abs,m_sbc_dir,m_sbc_imm,m_sbc_s,m_tdc,m_tsc,m_tsb_dir,m_tsb_abs:
       aRegister.condition := regUnknown;
 
    m_ldy_absX,m_ldy_dirX,m_ply:
@@ -1216,7 +1216,7 @@ var
              m_cmp_long,m_cmp_s,m_dea,m_eor_abs,m_eor_dir,m_eor_imm,m_eor_s,
              m_ina,m_lda_abs,m_lda_absx,m_lda_dir,m_lda_dirx,m_lda_imm,
              m_lda_indl,m_lda_indly,m_lda_long,m_lda_longx,m_lda_s,m_lsr_a,
-             m_ora_abs,m_ora_dir,m_ora_dirX,m_ora_imm,m_ora_longX,m_ora_s,
+             m_ora_abs,m_ora_dir,m_ora_dirX,m_ora_imm,m_ora_long,m_ora_longX,m_ora_s,
              m_pha,m_sbc_abs,m_sbc_dir,m_sbc_imm,m_sbc_s,m_sta_abs,m_sta_absX,
              m_sta_dir,m_sta_dirX,m_sta_indl,m_sta_indlY,m_sta_long,m_sta_longX,
              m_sta_s,m_tax,m_tay,m_tcd,m_tcs,m_xba,m_tsb_dir,m_tsb_abs] then
